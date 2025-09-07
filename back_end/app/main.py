89872e6fa18a2787fcfb3dee03775c2fd8055921
jake <eppi001004@gmail.com>
2025-09-07 10:10:16 +0900

    check

--- back_end/app/main.py
+++ back_end/app/main.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import Session
 import torch
 
 from app import models, schemas, crud, database
-from app.text_analysis import extract_pos_frequencies
+from app.text_analysis import extract_pos_frequencies, analyze_keyword_attention
 from app.database import Base, engine
-from app.nlp.model import KoKeyBERT
+from app.nlp.distillation_experiment.distill_model import DistillKoKeyBERT
 from app.nlp.kobert_tokenizer.kobert_tokenizer import KoBERTTokenizer
 from app.nlp.utils.extract import extract_keywords_from_bio_tags
 from transformers import BertConfig
@@ -26,9 +26,10 @@ app.add_middleware(
 )
 
 # 모델 초기화
-config = BertConfig.from_pretrained('skt/kobert-base-v1')
-model = KoKeyBERT(config=config)
-model.load_state_dict(torch.load('app/nlp/best_model.pt', map_location=torch.device('cpu'), weights_only=True))
+checkpoint = torch.load('app/nlp/distill_KoKeyBERT.pt', map_location=torch.device('cpu'), weights_only=False)
+config = checkpoint['model_config']
+model = DistillKoKeyBERT(config=config)
+model.load_state_dict(checkpoint['model_state_dict'])
 tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
 model.eval()
 
@@ -44,8 +45,15 @@ def extract_keywords(text: str = Form(...), db: Session = Depends(database.get_d
     nouns = list(pos_result['nouns'].keys())
     verbs = list(pos_result['verbs'].keys())
     adjectives = list(pos_result['adjectives'].keys())
-    keywords = extract_keywords_from_text(text)
-    print("추출된 키워드:", keywords)
+    keywords, outputs = extract_keywords_from_text(text)
+    attention_analysis_result = analyze_keyword_attention(
+        text=text,
+        keywords_info=keywords,
+        attentions=outputs.attentions,
+        tokenizer=tokenizer
+    )
+    logger.info(f"추출된 키워드: {keywords}")
+    logger.info(f"키워드별 어텐션 분석 결과: {attention_analysis_result}")
 
     pos_result_db = schemas.POSResultCreate(
         noun=", ".join(nouns),
